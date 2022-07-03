0c69755aeeee0be29f1419f8d83bbb0743e23c2c
guslave <88007915+guslave@users.noreply.github.com>
2022-07-04 09:45:36 +1200

    najwdbwad

--- THe doom game/The doom copy/EnemyBasicGrunt.gd
+++ THe doom game/The doom copy/EnemyBasicGrunt.gd
@@ -3,8 +3,8 @@ extends KinematicBody
 onready var nav = get_tree().get_nodes_in_group("NavMesh")[0]
 onready var player = get_tree().get_nodes_in_group("Player")[0]
 
-var path = []
-var path_index = 0
+var path = [] #hold the path coordinates from the enemy to the player
+var path_index = 0 #keep track of witch coordinate to go to
 var speed = 3
 var health = 20
 
@@ -16,22 +16,20 @@ func take_damage(dmg_amount):
 	if health <= 0:
 		death()
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	if path_index < path.size():
 		var direction = (path[path_index] - global_transform.origin)
 		if direction.length() < 1:
 			path_index += 1
 		else:
-			move_and_slide(direction.nrmalized() * speed, Vector3.UP)
+			move_and_slide(direction.normalized() * speed, Vector3.UP)
 	else:
 		find_path(player.global_transform.origin)
 
 func find_path(target):
-	path = nav.get_simple_path(global_transform.origin)
-	path = 0
-	
-	
-	
+	path = nav.get_simple_path(global_transform.origin,target)
+	path_index = 0
+
 func death():
 	set_process(false)
 	set_physics_process(false)
@@ -41,5 +39,5 @@ func death():
 	else:
 		$AnimatedSprite3D.play("die")
 
-func shoot(_target):
+func shoot(target):
 	pass
